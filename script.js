291c66d95f854ca35e9258241a8e37d52d19b115
Nic <nicisme@gmail.com>
2021-10-28 20:40:42 +0200

    Added Margin % Input and Project Name

--- script.js
+++ script.js
@@ -15,31 +15,33 @@ btn.addEventListener ('click', function () {
 			num7 = Number ( document.querySelector('#num7').value ),
 			num9 = Number ( document.querySelector('#num9').value ),
 			// The Element to show result in
-			rlt = document.querySelector('p.answer');
+			rlt = document.querySelector('p.answer'),
+
+			project = Number ( document.querySelector('#project') ),
+			margin = Number ( document.querySelector('#margin').value );
 
 
 
 
-	var a = num1 * num2;
-	var b = num1 + num2 + num3 + num4 + num5;
-	var c = b / (1-0.5);
-	var e = c - b;
+	var labor = num1 * num2;
+	var cos = labor + num4 + num5;
+	var selling = (cos / (1 - (margin/100))).toFixed(2);
+	var profit = (selling - cos).toFixed(2);
 
 
 
-	document.getElementById("num3").value = a;
-	document.getElementById("num6").value = b;
-	document.getElementById("num7").value = c;
-	document.getElementById("num9").value = e;
+	document.getElementById("num3").value = labor;
+	document.getElementById("num6").value = cos;
+	document.getElementById("num7").value = selling;
+	document.getElementById("num9").value = profit;
 	
 	// Get a checked radio/option button's value.
 	// input => tag name.
 	// [name="method"] => tag's attribte name and value.
 	// :checked => pseudo rule, only find element with checked state.
 	// .value => to get a value of that input.
-	var answer = e;
 	
 	
 	// Set content of HTML element.
-	rlt.innerHTML = answer;
+	rlt.innerHTML = profit;
 });
